1cef858f2055fbbcc61a1569fffceabb15f66c4f
Brandon Moye <85130587+Brandon-Moye@users.noreply.github.com>
2022-04-11 15:33:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const list = document.querySelector(".list");
 
 //Event Listeners
 submit.addEventListener("click", addActionItem);
+list.addEventListener("click", completedOrRemoveItem);
 //Functions
 function addActionItem(event) {
   //stopping the function from firing/reloading automatically
@@ -21,13 +22,26 @@ function addActionItem(event) {
   list.appendChild(actionItemContainer);
 
   const completeButton = document.createElement("button");
-  completeButton.innerHTML = "Complete";
+  completeButton.innerHTML = '<i class="fa-solid fa-check"></i>';
   completeButton.classList.add("completeButton");
-  newActionItem.appendChild(completeButton);
+  actionItemContainer.appendChild(completeButton);
 
   const deleteButton = document.createElement("button");
-  deleteButton.innerHTML = "Delete";
+  deleteButton.innerHTML = '<i class="fa-solid fa-minus"></i>';
   deleteButton.classList.add("deleteButton");
-  newActionItem.appendChild(deleteButton);
+  actionItemContainer.appendChild(deleteButton);
   input.value = ""; //clearning the form after submission
+}
+
+function completedOrRemoveItem(e) {
+  const item = e.target;
+  //DELETE ACTION ITEM
+  if (item.classList[0] === "deleteButton") {
+    item.parentElement.remove();
+  }
+  //COMPLETE ACTION ITEM
+  if (item.classList[0] === "completeButton") {
+    item.parentElement.classList.toggle("completed");
+    console.log(item.parentElement);
+  }
 }
